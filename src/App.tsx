164b8c1911a385c621c75964bc8cf7596a8a936d
v6l6nd <marinyu32@gmail.com>
2022-08-19 16:16:28 +0300

    readyBranch

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Wrapper } from './components/Wrapper';
+import { MainComponent } from './components/MainComponent/MainSlide';
+import { FullPost } from './components/FullPost/FullPost';
+import { CreatePost } from './components/CreatePost/CreatePost';
+import { LoginComponent } from './components/Auth/Login';
+import { RegistrComponent } from './components/Auth/Regist';
+import { TagsComponent } from './components/Tags/TagsComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Routes>
+    <Route path='/' element={<Wrapper/>}>
+    <Route index element={<MainComponent/>}/>
+    <Route path='/fullpost/:id' element={<FullPost/>}/>
+    <Route path='/fullpost/:id/edit' element={<CreatePost/>}/>
+    <Route path='/createpost' element={<CreatePost/>}/>
+    <Route path='/tags' element={<TagsComponent/>}/>
+    <Route path='/login' element={<LoginComponent/>}/>
+    <Route path='/registr' element={<RegistrComponent/>}/>
+    </Route>
+   </Routes>
   );
 }
 
